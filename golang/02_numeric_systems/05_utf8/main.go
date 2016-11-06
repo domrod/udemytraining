f11c18346682e7fc902e598ec832c3068598b5a1
Dominique Rodrigues <dominique.rodrigues.contact@gmail.com>
2016-11-06 22:59:47 +0100

    Added a comment in UTF-8 example

--- golang/02_numeric_systems/05_utf8/main.go
+++ golang/02_numeric_systems/05_utf8/main.go
@@ -7,6 +7,7 @@ import (
 func main() {
 	const limit = 256
 	for i := 0; i < limit; i++ {
+		// Nota: %q stands for UTF-8 notation
 		fmt.Printf("Decimal: %d \t Binary: %b \t Hexadecimal: %#x \t UTF8: %q \n", i, i, i, i)
 	}
 
