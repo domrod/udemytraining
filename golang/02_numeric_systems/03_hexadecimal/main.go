59789fa07e0e0cf6c91637acfda6101223a00534
Dominique Rodrigues <dominique.rodrigues.contact@gmail.com>
2016-11-06 22:37:58 +0100

    Hexadecimal in lower or upper case (%#x or %#X)

--- golang/02_numeric_systems/03_hexadecimal/main.go
+++ golang/02_numeric_systems/03_hexadecimal/main.go
@@ -5,7 +5,7 @@ import (
 )
 
 func main() {
-	// Nota: For hexadecimal, 2 ways => %x or %#x (with 0X notation)
+	// Nota: For hexadecimal, 2 ways => %x or %#x (with 0x (%#x) or 0X (%#X) notation)
 	// fmt.Printf("Decimal: %d - Binary: %b - Hexadecimal: %x \n", 42, 42, 42)
-	fmt.Printf("Decimal: %d \t - Binary: %b \t - Hexadecimal: %#x \n", 42, 42, 42)
+	fmt.Printf("Decimal: %d \t - Binary: %b \t - Hexadecimal: %#X \n", 42, 42, 42)
 }
