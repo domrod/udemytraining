4d03fae77d5db9fedd47f2fe8bc5237654b544ad
Dominique Rodrigues <dominique.rodrigues.contact@gmail.com>
2016-11-06 21:24:28 +0100

    2 representations for Hexadecimal

--- golang/02_numeric_systems/03_hexadecimal/main.go
+++ golang/02_numeric_systems/03_hexadecimal/main.go
@@ -5,6 +5,7 @@ import (
 )
 
 func main() {
-
-	fmt.Printf("Decimal: %d - Binary: %b - Hexadecimal: %x \n", 42, 42, 42)
+	// Nota: For hexadecimal, 2 ways => %x or %#x (with 0X notation)
+	// fmt.Printf("Decimal: %d - Binary: %b - Hexadecimal: %x \n", 42, 42, 42)
+	fmt.Printf("Decimal: %d \t - Binary: %b \t - Hexadecimal: %#x \n", 42, 42, 42)
 }
