d039b81c4561912cbcd7be94863875e90065edf2
Dominique Rodrigues <dominique.rodrigues.contact@gmail.com>
2016-11-07 00:36:21 +0100

    Renamed revervetwo in reversefunc

--- golang/03_package/stringutil/reverse.go
+++ golang/03_package/stringutil/reverse.go
@@ -3,5 +3,5 @@ package stringutil
 // Nota: Upper case for this function means its visibility is possible
 // Ouside this folder
 func Reverse(s string) string {
-	return reversetwo(s)
+	return reversefunc(s)
 }
--- golang/03_package/stringutil/reversefunc.go
+++ golang/03_package/stringutil/reversefunc.go
@@ -1,7 +1,7 @@
 package stringutil
 
 // Nota: lower case for this function means is visibility is local
-func reversetwo(s string) string {
+func reversefunc(s string) string {
 	r := []rune(s)
 	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
 		r[i], r[j] = r[j], r[i]
