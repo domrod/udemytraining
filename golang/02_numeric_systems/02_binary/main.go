aaf27a2a2166ec4bebd060ff86389472459788aa
Dominique Rodrigues <dominique.rodrigues.contact@gmail.com>
2016-11-06 21:12:49 +0100

    Added Numeric systems examples

---
+++ golang/02_numeric_systems/02_binary/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Printf("Decimal: %d - Binary: %b", 42, 42)
+}
---
+++ golang/02_numeric_systems/03_hexadecimal/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	fmt.Printf("Decimal: %d - Binary: %b - Hexadecimal: %x", 42, 42, 42)
+}
