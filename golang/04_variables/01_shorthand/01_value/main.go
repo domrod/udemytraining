eed82c2d0b32da2a65c2f579cc43452048a7820b
Dominique Rodrigues <dominique.rodrigues.contact@gmail.com>
2016-11-07 02:01:01 +0100

    Added variables examples

---
+++ golang/04_variables/01_shorthand/01_value/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	a := 0.7
+	b := "golang"
+	c := 32
+	d := true
+
+	fmt.Printf("%v \n", a)
+	fmt.Printf("%v \n", b)
+	fmt.Printf("%v \n", c)
+	fmt.Printf("%v \n", d)
+
+}
