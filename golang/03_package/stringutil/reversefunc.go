facd513f59ee956271461148ccd121e445250f3b
Dominique Rodrigues <dominique.rodrigues.contact@gmail.com>
2016-11-21 05:34:55 +0100

    Updated comments about rune in 03_package

--- golang/03_package/stringutil/reversefunc.go
+++ golang/03_package/stringutil/reversefunc.go
@@ -3,10 +3,10 @@ package stringutil
 // Nota: lower case for this function means is visibility is local
 func reversefunc(s string) string {
 	// Nota: https://golang.org/doc/go1#rune => rune is a Unicode type (any special character may then be handled)
-	r := []rune(s) // s converted into a run array r
+	r := []rune(s) // s converted into an array of int32 (rune type) => utf8 classification
 	// Reversing algorithm 2 by 2 (starting at extremeties up to the middle of the array)
 	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
 		r[i], r[j] = r[j], r[i] // parallel processing on 2 array elements at a time
 	}
-	return string(r) // rune type is returned into a string format
+	return string(r) // r is converted back into a string from utf8 classification
 }
